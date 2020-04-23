9a70c5e81fe2219ea0956b8301169cbe07715807
rarean <db.robot@yaoo.com>
2020-04-23 00:06:22 -0500

    initial commit

---
+++ src/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <root>
+      <app-navigation></app-navigation>
+    </root>
+</template>
+
+
+<script>
+import React from "react";
+import {
+  createAppContainer,
+  createStackNavigator,
+  createDrawerNavigator
+  } from "vue-native-router";
+import { Root } from "native-base";
+import LoginScreen from "./screen/login.vue";
+import HomeScreen from "./screen/home.vue";
+import Sidebar from "./screen/sidebar.vue";
+
+const Drawer = createDrawerNavigator(
+  {
+    Home: { screen: HomeScreen },
+  },
+  {
+    initialRouteName: "Home",
+    contentOptions: {
+      activeTintColor: "#e91e63"
+    },
+    contentComponent: props => <Sidebar {...props}/>
+  }
+);
+
+const AppNavigation = createAppContainer(
+  createStackNavigator(
+    {
+      Login: { screen: LoginScreen },
+      Drawer: { screen: Drawer }
+    },
+    {
+      initialRouteName: "Login",
+      headerMode: "none"
+    }
+  )
+);
+export default {
+  components: { Root, AppNavigation }
+};
+</script>
---
+++ src/screen/home.vue
@@ -0,0 +1,64 @@
+<template>
+    <nb-container :style="{flex:1, backgroundColor: '#fff'}">
+        <nb-header>
+          <nb-left>
+            <nb-button transparent
+              :on-press="() => this.props.navigation.openDrawer()" >
+                <nb-icon name="menu" />
+            </nb-button>
+          </nb-left>
+          <nb-body>
+                <nb-title>Posts</nb-title>
+            </nb-body>
+            <nb-right />
+        </nb-header>
+        <nb-content>
+            <nb-list>
+                <!--item
+                    v-if="!loading"
+                    v-for="(itemType, types) in items"
+                    :data="itemType" />
+                <nb-spinner v-if="loading"></nb-spinner-->
+            </nb-list>
+        </nb-content>
+    </nb-container>
+</template>
+
+<script>
+import React from 'react';
+import Item from '../components/item';
+import { Dimensions } from 'react-native';
+import store from '../store';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+export default {
+  computed: {
+    items () {
+      console.log("items", store.state.posts);
+      return store.state.posts;
+    },
+    loading () {
+      console.log("loading", store.state.loadingPosts);
+        return store.state;
+    }
+  },
+  props: {
+      navigation: Object
+  },
+  created () {
+    console.log("created",store.state);
+    //this.fetchList(store.state.activeType);
+  },
+  methods: {
+    fetchList (type) {
+      return store.dispatch('FETCH_LIST_DATA', {
+        type: type
+      });
+    }
+  },
+  components: {
+      Item
+  }
+};
+</script>
