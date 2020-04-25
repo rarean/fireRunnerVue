69e7159f9d21b7b6cc9339a3a9eaebdd6838c074
rarean <db.robot@yaoo.com>
2020-04-25 15:16:12 -0500

    prettier format

--- src/components/inputText.vue
+++ src/components/inputText.vue
@@ -2,28 +2,24 @@
   <nb-item floatingLabel>
     <nb-label>{{ label }}</nb-label>
     <nb-input v-bind="$attrs" v-bind:value="value" v-on:"inputListeners" />
-   </nb-item>
+  </nb-item>
 </template>
 
 <script>
 export default {
   inheritAttrs: false,
-  props: ['label', 'value'],
-  computed:{
-    inputListeners: function(){
+  props: ["label", "value"],
+  computed: {
+    inputListeners: function () {
       var that = this;
-      return Object.assign({},
-        this.$listeners,
-        {
-          input: function(event){
-            that.$emit('input', event.target.value)
-          }
+      return Object.assign({}, this.$listeners, {
+        input: function (event) {
+          that.$emit("input", event.target.value);
         }
-      )
+      });
     }
   }
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- src/screen/home.vue
+++ src/screen/home.vue
@@ -22,70 +22,62 @@
                     :data="itemType" />
                 <nb-spinner v-if="loading"></nb-spinner>
             </nb-list-->
-        <nb-content class="container">
-        <nb-form>
-          <input-text label="Date" v-model="dateValue" />
-          <input-text label="Incident#" v-model="incident"/>
-          <input-text label="Incident Reported:" v-model="inciReported"/>
-          <input-text label="Personnel#" v-model="personnelNum"/>
-          <input-text label="Medic Unit(s)" v-model="medicUnit"/>
-          <input-text label="Situation Found#" v-model="situation"/>
-        </nb-form>
-        <view class="btn">
-          <nb-button rounded large on-press="save">
-            <nb-text>Save</nb-text>
-           </nb-button>
-        </view>
-        </nb-content>
-    </nb-container>
+    <nb-content class="container">
+      <nb-form>
+        <input-text label="Date" v-model="dateValue" />
+        <input-text label="Incident#" v-model="incident" />
+        <input-text label="Incident Reported:" v-model="inciReported" />
+        <input-text label="Personnel#" v-model="personnelNum" />
+        <input-text label="Medic Unit(s)" v-model="medicUnit" />
+        <input-text label="Situation Found#" v-model="situation" />
+      </nb-form>
+      <view class="btn">
+        <nb-button rounded large on-press="save">
+          <nb-text>Save</nb-text>
+        </nb-button>
+      </view>
+    </nb-content>
+  </nb-container>
 </template>
 
 <script>
-import React from 'react';
-import Item from '../components/item';
-import { Dimensions } from 'react-native';
+import React from "react";
+import Item from "../components/item";
+import { Dimensions } from "react-native";
 import Title from "../components/title";
 import InputText from "../components/inputText";
-import store from '../store';
+import store from "../store";
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default {
   components: { Item, Title, InputText },
   props: { navigation: Object },
-  data: function(){
+  data: function () {
     return {
-      dateValue:'',
-      incident:'',
-      inciReported:'',
-      personnelNum:'',
-      medicUnit:'',
-      situation:''
+      dateValue: "",
+      incident: "",
+      inciReported: "",
+      personnelNum: "",
+      medicUnit: "",
+      situation: ""
     };
   },
   computed: {
-    titleName(){
+    titleName() {
       return this.navigation.state.routeName;
-    },
-    //items () {
-    //  console.log("items", store.state.posts);
-    //  return store.state.posts;
-    //},
-    //loading () {
-    //  console.log("loading", store.state.loadingPosts);
-    //    return store.state;
-    //}
+    }
   },
-  created () {
-    console.log("created",store.state);
+  created() {
+    console.log("created", store.state);
     console.log("created2", this.navigation);
     //this.fetchList(store.state.activeType);
   },
   methods: {
-    fetchList (type) {
-      return store.dispatch('FETCH_LIST_DATA', { type: type });
+    fetchList(type) {
+      return store.dispatch("FETCH_LIST_DATA", { type: type });
     },
-    onHandleMenu(){
+    onHandleMenu() {
       console.log("handleMenu");
     }
   }
@@ -93,39 +85,39 @@ export default {
 </script>
 
 <style>
-  .container {
-    margin: 20;
-    padding: 20;
-  }
-  .btn {
-    margin: 20;
-    padding: 20;
-    justify-content: center;
-    align-items: center;
-  }
-  .score {
-    width: 40;
-    height: 40;
-    background-color: #fff;
-    border-radius: 20;
-    justify-content: center;
-    align-items: center;
-    margin-right: 16;
-  }
-  .score-text {
-    color: #ff6600;
-    font-weight: bold;
-  }
-  .detail {
-    flex: 1
-  }
-  .name {
-    color: #666;
-    font-size: 12;
-    margin-bottom: 6;
-  }
-  .title {
-    color: #333;
-    font-size: 14;
-  }
+.container {
+  margin: 20;
+  padding: 20;
+}
+.btn {
+  margin: 20;
+  padding: 20;
+  justify-content: center;
+  align-items: center;
+}
+.score {
+  width: 40;
+  height: 40;
+  background-color: #fff;
+  border-radius: 20;
+  justify-content: center;
+  align-items: center;
+  margin-right: 16;
+}
+.score-text {
+  color: #ff6600;
+  font-weight: bold;
+}
+.detail {
+  flex: 1;
+}
+.name {
+  color: #666;
+  font-size: 12;
+  margin-bottom: 6;
+}
+.title {
+  color: #333;
+  font-size: 14;
+}
 </style>
